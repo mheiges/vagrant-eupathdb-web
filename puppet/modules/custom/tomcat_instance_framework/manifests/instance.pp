7a3dc55a4f2a0003683ededd5bf0ae0675c8b5e7
Mark Heiges <mheiges@uga.edu>
2015-07-13 16:25:29 -0400

    handle ensure:absent

--- puppet/modules/custom/tomcat_instance_framework/manifests/instance.pp
+++ puppet/modules/custom/tomcat_instance_framework/manifests/instance.pp
@@ -1,4 +1,4 @@
-# Create an instance of Tomcat.
+# Manage an instance of Tomcat.
 # Requires puppetlabs/stdlib module for parameter validation.
 
 define tomcat_instance_framework::instance (
@@ -16,79 +16,100 @@ define tomcat_instance_framework::instance (
   $environment = undef,
 ) {
 
-  if $instance_name == undef { fail("'instance_name' is not defined") }
-  if $http_port == undef { fail("'http_port' is not defined") }
-  if $ajp13_port == undef { fail("'ajp13_port' is not defined") }
-  #if $jmx_port == undef { fail("'jmx_port' is not defined") }
-  if $tomcat_user == undef { fail("'tomcat_user' is not defined") }
-  if $template_ver == undef { fail("'template_ver' is not defined") }
-
-  if $orcl_jdbc_path {
-    validate_absolute_path($orcl_jdbc_path)
+  $service_state = $ensure ? {
+    'absent' => 'stopped',
+    default  => $ensure
   }
 
-  if $pg_jdbc_path {
-    validate_absolute_path($pg_jdbc_path)
-  }
+  if ( $ensure == 'absent' ) {
+    exec { "delete-active-${name}":
+      command => "rm -rf ${instances_dir}/${name}",
+      path    => '/usr/bin:/bin',
+      onlyif  => "test -d ${instances_dir}/${name}",
+      require => Service["tcif-${name}"],
+    }
+    exec { "delete-inactive-${name}":
+      command => "rm -rf ${instances_dir}/_${name}",
+      path    => '/usr/bin:/bin',
+      onlyif  => "test -d ${instances_dir}/_${name}",
+      require => Service["tcif-${name}"],
+    }
+  } else {
 
-  if $instances_dir {
-    validate_absolute_path($instances_dir)
-  }
+    if $instance_name == undef { fail("'instance_name' is not defined") }
+    if $http_port == undef { fail("'http_port' is not defined") }
+    if $ajp13_port == undef { fail("'ajp13_port' is not defined") }
+    #if $jmx_port == undef { fail("'jmx_port' is not defined") }
+    if $tomcat_user == undef { fail("'tomcat_user' is not defined") }
+    if $template_ver == undef { fail("'template_ver' is not defined") }
 
-  $make_cmd = "make install                     \
-    INSTANCE=${instance_name}                   \
-    HTTP_PORT=${http_port}                      \
-    AJP13_PORT=${ajp13_port}                    \
-    JMX_PORT=${jmx_port}                        \
-    TOMCAT_USER=${tomcat_user}                  \
-    TEMPLATE=${template_ver}
-  "
-  exec { "make-${instance_name}":
-    command => $make_cmd,
-    path    => ['/usr/local/bin', '/bin', '/usr/bin'],
-    cwd     => $instances_dir,
-    unless  => "test -d ${instances_dir}/_${name}",
-    creates => "${instances_dir}/${instance_name}",
-  }
+    if $orcl_jdbc_path {
+      validate_absolute_path($orcl_jdbc_path)
+    }
 
-  if ( $ensure == 'running' ) {
-    file { "${name}-env":
-      path    => "${instances_dir}/${name}/conf/instance.env",
-      owner   => $tomcat_user,
-      group   => 'tomcat',
-      mode    => '0644',
-      content => template('tomcat_instance_framework/instance.env.erb'),
-      require => [Exec["make-${instance_name}"]],
-      notify  => Service["tcif-${name}"]
+    if $pg_jdbc_path {
+      validate_absolute_path($pg_jdbc_path)
+    }
+
+    if $instances_dir {
+      validate_absolute_path($instances_dir)
+    }
+
+    $make_cmd = "make install                     \
+      INSTANCE=${instance_name}                   \
+      HTTP_PORT=${http_port}                      \
+      AJP13_PORT=${ajp13_port}                    \
+      JMX_PORT=${jmx_port}                        \
+      TOMCAT_USER=${tomcat_user}                  \
+      TEMPLATE=${template_ver}
+    "
+    exec { "make-${instance_name}":
+      command => $make_cmd,
+      path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+      cwd     => $instances_dir,
+      unless  => "test -d ${instances_dir}/_${name}",
+      creates => "${instances_dir}/${instance_name}",
+    }
+
+    if ( $ensure == 'running' ) {
+      file { "${name}-env":
+        path    => "${instances_dir}/${name}/conf/instance.env",
+        owner   => $tomcat_user,
+        group   => 'tomcat',
+        mode    => '0644',
+        content => template('tomcat_instance_framework/instance.env.erb'),
+        require => [Exec["make-${instance_name}"]],
+        notify  => Service["tcif-${name}"]
+      }
+    }
+
+    if ( $ensure == 'stopped' ) {
+      exec { "disable-${name}":
+        path    => '/usr/bin:/bin',
+        command => "mv ${instances_dir}/${name} ${instances_dir}/_${name}",
+        creates => "${instances_dir}/_${name}",
+        require => [Exec["make-${instance_name}"], Service["tcif-${name}"]],
+      }
+    }
+
+    if ( $ensure == 'running' ) {
+      exec { "enable-${name}":
+        command => "mv ${instances_dir}/_${name} ${instances_dir}/${name}",
+        path    => '/usr/bin:/bin',
+        onlyif  => "test -d ${instances_dir}/_${name}",
+        creates => "${instances_dir}/${name}",
+        notify  => Service["tcif-${name}"],
+        before  => Exec["make-${instance_name}"],
+      }
     }
   }
 
   service { "tcif-${name}":
-    ensure  => $ensure,
+    ensure  => $service_state,
     start   => "instance_manager start ${name}",
     stop    => "instance_manager stop ${name} force",
     restart => "instance_manager restart ${name}",
     status  => "instance_manager status ${name} | grep -q '${name}'",
-  }
-
-  if ( $ensure == 'stopped' ) {
-    exec { "disable-${name}":
-      path    => '/usr/bin:/bin',
-      command => "mv ${instances_dir}/${name} ${instances_dir}/_${name}",
-      creates => "${instances_dir}/_${name}",
-      require => [Exec["make-${instance_name}"], Service["tcif-${name}"]],
-    }
-  }
-
-  if ( $ensure == 'running' ) {
-    exec { "enable-${name}":
-      command => "mv ${instances_dir}/_${name} ${instances_dir}/${name}",
-      path    => '/usr/bin:/bin',
-      onlyif  => "test -d ${instances_dir}/_${name}",
-      creates => "${instances_dir}/${name}",
-      notify  => Service["tcif-${name}"],
-      before  => Exec["make-${instance_name}"],
-    }
   }
 
 }
